ab3e46bee3246770f83c254e310557fc54935153
Sumanth Kamath <145032252+S-Kamath-01@users.noreply.github.com>
2024-10-30 15:15:50 +0530

    pointers basics

---
+++ Pointers/pointers_tutorials.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num = 5;
+    cout<< num << endl;
+    // address of operator - &
+    cout<<"address of operator is "<<&num<<endl;
+    int *ptr = & num;
+    (*ptr)++;
+    cout<<num<<endl;
+    // it is the same as num++
+    //* is the dereference operator and *ptr means the value at address pointed by ptr
+    cout<<*ptr<<endl;
+    
+    return 0;
+    
+}
